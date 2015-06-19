877186c94f6c76daaecec5a9fa6259c5a52ac37b
Kris Dockter <kriskd@gmail.com>
2015-06-19 17:37:48 -0500

    Hash password

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * User Entity.
@@ -24,4 +25,8 @@ class User extends Entity
         'instructors' => true,
         'studios' => true,
     ];
+
+	protected function _setPassword($password) {
+		return (new DefaultPasswordHasher)->hash($password);
+    }
 }
